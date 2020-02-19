b57b0a937cb3716f8184e4c5b78b56d261b67409
essential-workshop-9972 <josephjones7@gmail.com>
2020-02-19 17:23:11 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,11 @@
+puts "Hello World\n"
+Hello World
+ => nil
+ 
+print "Hello World\n"
+Hello World
+ => nil
+ 
+p "Hello World\n"
+"Hello World"
+ => "Hello World\n"
---
+++ test.rb
@@ -0,0 +1,8 @@
+case greeting
+  when "unfriendly_greeting"
+   puts "what do you want!?"
+  when "friendly_greeting"
+    puts "hi ! How are you?"
+ 
+  
+end
